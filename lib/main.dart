04b69a37e1b189f2a89f7c947b9354a4a760e5a9
Allison Piovani <apiovani25@gmail.com>
2020-06-24 22:59:54 -0300

    Add variable to text and value

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,10 @@ class ExpensesApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
+
+  final titleController = TextEditingController();
+  final valueController = TextEditingController();
+
   final _transactions = [
     Transaction(
       id: 't1',
@@ -88,9 +92,11 @@ class MyHomePage extends StatelessWidget {
               child: Column(
                 children: <Widget>[
                   TextField(
+                    controller: titleController,
                     decoration: InputDecoration(labelText: 'Titulo'),
                   ),
                   TextField(
+                    controller: valueController,
                     decoration: InputDecoration(labelText: 'Valor (R\$)'),
                   ),
                   Row(
@@ -98,8 +104,11 @@ class MyHomePage extends StatelessWidget {
                     children: <Widget>[
                       FlatButton(
                         child: Text('Nova Transação'),
-                        textColor: Cologrs.purple,
-                        onPressed: () {},
+                        textColor: Colors.purple,
+                        onPressed: () {
+                          print(titleController.text);
+                          print(valueController.text);
+                        },
                       ),
                     ],
                   )
