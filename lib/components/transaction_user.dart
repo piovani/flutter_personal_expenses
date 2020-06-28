d8dc3518105c46b60f13d898c2cdcb05eefa10c0
Allison Piovani <apiovani25@gmail.com>
2020-06-27 23:47:11 -0300

    Component refactoring 2

--- lib/components/transaction_user.dart
+++ lib/components/transaction_user.dart
@@ -1,8 +1,54 @@
-class TransactionUser extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'package:flutter_personal_expenses/components/transaction_list.dart';
+import 'transactions_form.dart';
+import 'transactions_form.dart';
+import '../models/transaction.dart';
+
+class TransactionUser extends StatefulWidget {
+  @override
+  _TransactionUserState createState() => _TransactionUserState();
+}
+
+class _TransactionUserState extends State<TransactionUser> with SingleTickerProviderStateMixin {
+  
+  AnimationController _controller;
+
+  final _transactions = [
+    Transaction(
+      id: 't1',
+      title: 'Novo Tênis',
+      value: 777.90,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't2',
+      title: 'Cacau show',
+      value: 15.50,
+      date: DateTime.now(),
+    )
+  ];
+
+
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(vsync: this);
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _controller.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Column(
+      children: <Widget>[
+        TransactionList(_transactions),
+        TransactionForm(),
+      ],
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,36 +1,18 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_personal_expenses/components/transaction_list.dart';
-import 'package:flutter_personal_expenses/components/transactions_form.dart';
-import 'package:flutter_personal_expenses/models/transaction.dart';
-import 'models/transaction.dart';
-
+import 'components/transaction_user.dart';
 
 main() => runApp(ExpensesApp());
 
 class ExpensesApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: MyHomePage());
+    return MaterialApp(
+      home: MyHomePage(),
+    );
   }
 }
 
 class MyHomePage extends StatelessWidget {
-
-  final _transactions = [
-    Transaction(
-      id: 't1',
-      title: 'Novo Tênis',
-      value: 777.90,
-      date: DateTime.now(),
-    ),
-    Transaction(
-      id: 't2',
-      title: 'Cacau show',
-      value: 15.50,
-      date: DateTime.now(),
-    )
-  ];
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -48,8 +30,7 @@ class MyHomePage extends StatelessWidget {
               elevation: 5,
             ),
           ),
-          TransactionList(_transactions),
-          TransactionForm(),
+          TransactionUser(),
         ],
       ),
     );
