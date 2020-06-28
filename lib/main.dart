c3a035d4cd25517115c0b4224d553975585b5fc0
Allison Piovani <apiovani25@gmail.com>
2020-06-28 01:11:08 -0300

    Remove transaction user

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,11 @@
 import 'package:flutter/material.dart';
-import 'components/transaction_user.dart';
+import 'package:flutter_personal_expenses/components/transactions_form.dart';
+import 'package:flutter_personal_expenses/models/transaction.dart';
+
+import 'dart:math';
+import 'components/transactions_form.dart';
+import 'components/transaction_list.dart';
+import './models/transaction.dart';
 
 main() => runApp(ExpensesApp());
 
@@ -12,7 +18,57 @@ class ExpensesApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  final _transactions = [
+    Transaction(
+      id: 't1',
+      title: 'Novo Tênis',
+      value: 777.90,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't2',
+      title: 'Cacau show',
+      value: 15.50,
+      date: DateTime.now(),
+    ),
+     Transaction(
+      id: 't3',
+      title: 'Cacau show',
+      value: 78.10,
+      date: DateTime.now(),
+    ), 
+  ];
+
+  _addTransaction(String title, double value) {
+    final newTransaction = Transaction(
+      id: Random().nextDouble().toString(),
+      title: title,
+      value: value,
+      date: DateTime.now(),
+    );
+
+    setState(() {
+      _transactions.add(newTransaction);
+    });
+  }
+
+  _openTransactionFormModal(BuildContext context) {
+    showModalBottomSheet(
+      context: context, 
+      builder: (_) {
+        return TransactionForm(null);
+      }
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -37,8 +93,13 @@ class MyHomePage extends StatelessWidget {
                 elevation: 5,
               ),
             ),
-            TransactionUser(),
-          ],
+            Column(
+              children: <Widget>[
+                TransactionForm(_addTransaction),
+                TransactionList(_transactions),
+              ],
+            )
+          ]
         ),
       ),
       floatingActionButton: FloatingActionButton(
