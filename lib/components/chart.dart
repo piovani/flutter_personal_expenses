9efc03821b2e8fa8a80aeb0701e51899fd07f258
Allison Piovani <apiovani25@gmail.com>
2020-07-08 21:27:13 -0300

    sum values and get percentage from week

--- lib/components/chart.dart
+++ lib/components/chart.dart
@@ -33,6 +33,12 @@ class Chart extends StatelessWidget {
     });
   }
 
+  double get _weekTotalValue {
+    return groupedTransactions.fold(0.0, (sum, tr) {
+      return sum + tr['value'];
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     groupedTransactions;
@@ -45,7 +51,7 @@ class Chart extends StatelessWidget {
           return ChartBar(
             label: tr['day'],
             value: tr['value'],
-            percentage: 0.3,
+            percentage: (tr['value'] as double) / _weekTotalValue,
           );
         }).toList(),
       ),
